12c4a1c63847306dca9dbf41bcc65ba6b522a565
kartavya <kartavyas@gmail.com>
2022-05-14 10:34:34 +1000

    updated query.sql but need fix the bug

--- db/query.sql
+++ db/query.sql
@@ -1,8 +1,21 @@
 SELECT 
-role_table.id AS role_id,
-role_table.title AS title,
-role_table.salary AS salary,
-department.department_name AS department_name
-FROM role_table
-JOIN department
-ON role_table.department_id = department.id;
+role_table.id AS Role_id,
+role_table.title AS Job_Title,
+role_table.salary AS Salary,
+department.department_name AS Department_name
+    FROM role_table
+    JOIN department
+        ON role_table.department_id = department.id;
+
+SELECT
+employee.id AS Employee_id,
+employee.first_name AS First_name,
+employee.last_name AS Last_name,
+role_table.title AS Job_Title,
+role_table.salary AS Salary,
+department.department_name AS Department_name,
+employee.first_name AS Manager_First_name,
+employee.last_name AS Manager_First_name
+    FROM employee AS employee
+    JOIN role_table ON employee.department_id = department.id
+    JOIN employee ON employee.manager_id = employee.id;
