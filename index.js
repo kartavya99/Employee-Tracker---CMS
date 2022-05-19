ef107cba4a6b11f59242a8ac2234bc554c927219
kartavya <kartavyas@gmail.com>
2022-05-19 21:39:50 +1000

    tidy up the code

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function userOptions () {
         }
     ])
     .then((answer) => {
-       // viewDept(); - withouth swtich statement get list of all depts
+       
         switch (answer.optionTree) {
             case "1 - View all Departments":
                 viewDept();
@@ -111,14 +111,6 @@ function userOptions () {
 
 
 // function to obtain list of all dept
-// async function viewDept() {
-//     await database.viewAllDept().then(([rows]) => {
-//         let dept = rows;
-//         console.log("\n");
-//         console.table(dept);
-//     }).then(() => userOptions());
-// };
-//converted above function into async
 async function viewDept() {
     const dept = await database.viewAllDept();
     const result = [...dept];
@@ -131,14 +123,6 @@ async function viewDept() {
 
 
 // function to obtain list of all roles
-// async function viewRoles () {
-//     await database.viewAllRoles().then(([rows]) => {
-//         let roles = rows;
-//         console.log("\n");
-//         console.table(roles);
-//     }).then(() => userOptions());
-// };
-// converting above into async functin
 async function viewRoles() {
     const roles = await database.viewAllRoles();
     const result = [...roles];
@@ -151,14 +135,6 @@ async function viewRoles() {
 
 
 // async function viewEmployee() {
-//     await database.viewAllEmployees().then(([rows]) => {
-//         let employee = rows;
-//         console.log("\n");
-//         console.table(employee);
-//     }).then(() => userOptions());
-// };
-
-// async function for better readbility 
 async function viewEmployee () {
     const employee = await database.viewAllEmployees();
     const result = [...employee];
@@ -168,51 +144,17 @@ async function viewEmployee () {
     userOptions();
 };
 
-
-
-
 //function to view managers
-// async function viewManagers() {
-//     await database.viewAllManagers().then(([rows])=>{
-//         let managers = rows;
-//         console.log("\n");
-//         console.table(managers);
-//     }).then(()=>userOptions());
-// };
-// converting above function into async 
 async function viewManagers() {
     const managers = await database.viewAllManagers();
     let result = [...managers];
     console.log("\n");
     console.table(result[0]);
     userOptions();
-
 };
 
 
 //function to add new department
-// function addDept () {
-
-//      inquirer.prompt ([
-//         {
-//             name: "DepName",
-//             message: "Please enter the name of the department",
-//             type: "input"
-//         }
-//     ])
-//     .then((department) => { 
-//         // console.log(department);
-//         database.addAllDept(department.DepName).then( (rows) => {
-//             let departments = rows;
-//             console.log(`Added ${department.DepName} to the databse.`);
-//             // console.table(viewAllDept());
-//         })
-//         .then(() => userOptions()); 
-        
-//     });
-    
-// };
-//converting above function into async
 async function addDept() {
     let answers = await prompt ([
         {
@@ -232,35 +174,7 @@ async function addDept() {
 };
 
 
-
 // function to add new Roles
-// function addRole () {
-//     return inquirer.prompt([
-//         {
-//             name: "title",
-//             message: "Please enter name of the role you would like to add ",
-//             type: "input"
-//         },
-//         {
-//             name: "dept",
-//             message:"Please enter the department ID",
-//             type: "number"
-//         },
-//         {
-//             name: "salary",
-//             message: "Please enter salary. (formate : 60000)",
-//             type: "number"
-//         }
-//     ])
-//     .then((role) => {
-//         database.addAllrole(role.title, role.dept, role.salary);
-//         console.log(`Added ${role.title} in dept ${role.dept} with ${role.salary} PA salary to the database`);
-//         console.table(viewRoles());
-        
-//     });
-           
-// };
-// converting above fucntion in async
 async function addRole () {
      let answers = await prompt([
         {
@@ -287,38 +201,6 @@ async function addRole () {
 };
 
 // function to add new employee
-// function addEmployee () {
-//     return inquirer.prompt([
-//         {
-//             name: "firstName",
-//             message: "Please Enter their first name",
-//             type: "input"
-//         },
-//         {
-//             name: "lastName",
-//             message: "Please Enter their last name",
-//             type: "input"
-//         },
-//         {
-//             name: "roleId",
-//             message: "Please select their role ID",
-//             type: "input"
-            
-//         },
-//         {
-//             name: "mangId",
-//             message: "Please select their manager ID",
-//             type: "input"
-            
-//         }
-
-//     ]).then((employee) => {
-//              database.addAllEmployee(employee.firstName, employee.lastName, employee.roleId, employee.mangId)
-//             console.log(`added ${employee.firstName} ${employee.lastName} with ${employee.roleId} and their manager id ${employee.mangId} to the database`);
-//             userOptions();
-//     });
-// };
-// converting addemployee function inbto async
 async function addEmployee () {
     let answers = await prompt([
         {
@@ -357,29 +239,6 @@ async function addEmployee () {
 };
 
 //function to updated employee
-// function updateEmployee() {
-    
-//     return inquirer.prompt ([
-//         {
-//             name:"id",
-//             message: "Please secect their new role ID" ,
-//             type: "input"
-                      
-//         },
-//         {
-//             name:"roleId",
-//             message: "Please secect their current employee ID",
-//             type: "input"
-            
-//         }
-//         ]).then ((employee) => {
-//         database.updateAllEmployee(employee.id, employee.roleId);
-//         console.log(`Updated ${employee.roleId} and ${employee.id} to the database`);
-//         userOptions();
-//     })
-
-// };
-// updating above function into async await
 async function updateEmployee() {
     
     let answers = await prompt ([
@@ -407,23 +266,7 @@ async function updateEmployee() {
         
 };
 
-
-
 // functionality to remove employee
-// function removeEmployee() {
-//     return inquirer.prompt ([
-//         {
-//            name:"rmEmp",
-//            message:"Please choose the employee ID you want to remove.",
-//            type: "input"
-//         }
-
-//     ]).then((employee) => {
-//         database.removeAllEmployee(employee.rmEmp);
-//         userOptions();
-//     })
-// };
-//converting above function to async 
 async function removeEmployee() {
     let answers = await prompt ([
         {
@@ -442,22 +285,7 @@ async function removeEmployee() {
    
 };
 
-
-
-// function to remove deparment through dept id
-// function removeDept () {
-//     return inquirer.prompt([
-//         {
-//             name:"rmDept",
-//             message:"Please choose the deparment ID you want to remove.",
-//             type: "input"
-//         }                  
-//     ]).then((dept) => {
-//         database.removeAlldept(dept.rmDept)
-//         userOptions();
-//     })
-// };
-// converting abvoe function into async await
+// function to remove department through dept id
 async function removeDept () {
     let answers = await prompt([
         {
@@ -473,23 +301,7 @@ async function removeDept () {
     userOptions();
        
 };
-
-
-
 // function to remove Role through Role id
-// function removeRole () {
-//     return inquirer.prompt([
-//         {
-//             name:"rmRole",
-//             message:"Please choose the deparment ID you want to remove.",
-//             type: "input"
-//         }                  
-//     ]).then((role) => {
-//         database.removeAllRole(role.rmRole);
-//         userOptions();       
-//     })
-// };
-//converting above function async await
 async function removeRole () {
     let answers = await prompt([
         {
@@ -506,7 +318,6 @@ async function removeRole () {
     userOptions();        
     
 };
-
 
 async function viewEmpByDept() {
     const employee = await database.viewAllEmployeesByDept();
